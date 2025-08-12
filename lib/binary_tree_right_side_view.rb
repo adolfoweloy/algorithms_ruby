866d7e26ca7137b140f35414b8a5c7eaf846213e
adolfoweloy <1496547+adolfoweloy@users.noreply.github.com>
2025-08-12 16:00:49 +1000

    Add DFS approach for binary tree right side view

--- lib/binary_tree_right_side_view.rb
+++ lib/binary_tree_right_side_view.rb
@@ -2,7 +2,8 @@
 # To run the test, execute bundle exec rspec ./spec/binary_tree_right_side_view_spec.rb
 class BinaryTreeRightSideView
   def right_side_view(root)
-    solution = BFSBinaryTreeRightSideView.new
+    #solution = BFSBinaryTreeRightSideView.new
+    solution = DFSBinaryTreeRightSideView.new
     solution.right_side_view(root)
   end
 end
@@ -27,6 +28,24 @@ class BFSBinaryTreeRightSideView < BinaryTreeRightSideView
 
       result << node.val
     end
+
+    result
+  end
+end
+
+# This solution is way more elegant and faster for large inputs
+class DFSBinaryTreeRightSideView < BinaryTreeRightSideView
+  def right_side_view(root)
+    self.dfs(root, 0, [])
+  end
+
+  def dfs(node, level, result)
+    return [] if node.nil?
+
+    result << node.val if result.size == level
+
+    dfs(node.right, level + 1, result)
+    dfs(node.left, level + 1, result)
 
     result
   end
